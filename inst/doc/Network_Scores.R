6a78fc9d22089ab2c45f1e2c540c87220bab0233
Hudson Golino <hfg9s@virginia.edu>
2020-01-18 23:20:03 +0000

    version 0.9.0

--- inst/doc/Network_Scores.R
+++ inst/doc/Network_Scores.R
@@ -1,16 +1,16 @@
-## ---- include = FALSE----------------------------------------------------
+## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#"
 )
 
-## ----install, echo = TRUE, eval = FALSE, message = FALSE, warning = FALSE----
+## ----install, echo = TRUE, eval = FALSE, message = FALSE, warning = FALSE-----
 #  # Install 'NetworkToolbox' package
 #  install.packages("NetworkToolbox")
 #  # Install 'lavaan' package
 #  install.packages("lavaan")
 
-## ----load, echo = TRUE, message = FALSE, warning = FALSE-----------------
+## ----load, echo = TRUE, message = FALSE, warning = FALSE----------------------
 # Load packages
 library(EGAnet)
 library(NetworkToolbox)
@@ -20,11 +20,11 @@ library(lavaan)
 # Run EGA
 ega <- EGA(neoOpen)
 
-## ----loadings, echo = TRUE, message = FALSE, warning = FALSE-------------
+## ----loadings, echo = TRUE, message = FALSE, warning = FALSE------------------
 # Standardized
 net.loads <- net.loads(A = ega)$std
 
-## ----scores, echo = TRUE, message = FALSE, warning = FALSE---------------
+## ----scores, echo = TRUE, message = FALSE, warning = FALSE--------------------
 # Network scores
 net.scores <- net.scores(data = neoOpen, A = ega)
 
